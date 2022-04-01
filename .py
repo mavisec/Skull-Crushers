ad73c7cf549ee2461fecdd0b65b1085d8c1fb861
Rishab Nayyar <82491413+mavisec@users.noreply.github.com>
2022-04-01 12:47:15 +0530

    Update and rename .py to r-u-p-o.py

--- .py
+++
@@ -1,8 +0,0 @@
-import sys
-
-f = open('100.txt','r')
-
-for i in f:
-    print("The Usernames in the file {} are: {}".format(sys.argv,i)) 
-
-f.seek(0)
---
+++ r-u-p-o.py
@@ -0,0 +1,7 @@
+import sys #importing sys module
+
+f = open('100.txt','r') #reading from the file
+
+for i in f:
+    print("The Usernames in the file {} are: {}".format(sys.argv,i)) #adding a custom line before the usernames or passwords.
+f.seek(0)
